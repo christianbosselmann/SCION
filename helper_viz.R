aa18fcfe5c1fe339beafaa5342d73d512d8713cc
christianbosselmann <christian.bosselmann@med.uni-tuebingen.de>
2022-05-25 13:49:19 +0200

    viz
    additional visualization for phenotypic weights and distance-based network graph

--- helper_viz.R
+++ helper_viz.R
@@ -21,7 +21,7 @@ names(files) <- basename(paths) %>%
   sub("([^.]+)\\.[[:alnum:]]+$", "\\1", .)
 
 # manual: labels
-labels <- c("Dirac", "Union", "MTL", "MKL")
+labels <- c("Uniform", "Global weights", "Hierarchical decomposition")
 load("t_vec.rda")
 gene_labels <- c("SCN1A", "SCN2A", "SCN3A", "SCN4A", "SCN5A", "SCN8A", "SCN9A", "SCN10A", "SCN11A")
 
@@ -35,12 +35,12 @@ for (i in 1:length(files)){
   auc_label <- list()
   auc_label[[i]] <- files[[i]] %>% 
     group_by(fold) %>%
-    roc_auc(truth = factor(truth, levels = c("GOF", "LOF")), GOF) %>%
+    roc_auc(truth = factor(truth, levels = c("LOF", "GOF")), LOF) %>%
     summarize(mean = format(round(mean(.estimate), digits = 3), nsmall = 3), 
               sd = format(round(sd(.estimate), digits = 3), nsmall = 3))
   
   roc[[i]] <- files[[i]] %>%
-    roc_curve(truth = factor(truth, levels = c("GOF", "LOF")), GOF) %>%
+    roc_curve(truth = factor(truth, levels = c("LOF", "GOF")), LOF) %>%
     ggplot(aes(x = 1-specificity, y = sensitivity)) +
     geom_path() +
     geom_abline(lty = 3) +
@@ -105,7 +105,7 @@ files %>%
   rbindlist(idcol = "model") %>%
   group_by(gene, fold, model) %>%
   accuracy(factor(truth, levels = c("GOF", "LOF")), 
-      factor(pred, levels = c("GOF", "LOF"))) %>%
+           factor(pred, levels = c("GOF", "LOF"))) %>%
   replace(is.na(.), 0) %>%
   group_by(gene, model) %>%
   summarize(mean = mean(.estimate)) %>%
@@ -121,3 +121,79 @@ files %>%
        y = "Accuracy") +
   scale_fill_brewer(palette = "Pastel1") +
   coord_fixed(ratio = 5, ylim = c(0,1), expand = FALSE, clip = "on")
+
+### noise plot for figure 4, fig.csv is found in output folder
+fig %>% 
+  ggplot(aes(x = log(noise), y = accuracy, 
+             fill = method, group = method)) + 
+  geom_line(aes(linetype = method, color = method)) +
+  geom_point(aes(shape = method, color = method)) +
+  geom_errorbar(aes(ymin = accuracy-sd, ymax = accuracy+sd, color = method), width=.1) +
+  theme_bw() +
+  scale_color_brewer(palette = "Dark2") +
+  coord_fixed(ratio = 8, xlim = c(-1.5, 1.5), ylim = c(0.69, 0.91), expand = FALSE, clip = "on") +
+  labs(x = "Log (Noise)",
+       y = "Accuracy")
+
+### dendrogram of weights for hierarchical decomposition MTMKL
+# requires distance matrix G and list d from hierarchical MTMKL containing weights
+# first calculate refined pairwise task similarity γk,l (see Widmer et al. 2010)
+df_2tasks <- setDT(CJ(names(G), names(G), unique = TRUE))
+# df_2tasks <- df_2tasks[!duplicated(t(apply(df_2tasks, 1, sort))),]
+
+mat <- matrix(data = NA, nrow = length(names(G)), ncol = length(names(G)))
+rownames(mat) <- names(G)
+colnames(mat) <- names(G)
+
+for (i in 1:nrow(df_2tasks)){
+  t <- as.character(df_2tasks[i,]) # get pairwise tasks
+  tmp <- sapply(d$graph, function(x) t %in% x) # get indices of nodes containing task
+  ind <- which(tmp[1,] & tmp[2,])
+  wt <- sum(d$delta[ind]) # sum weight of task set
+  mat[t[1], t[2]] <- wt
+}
+
+# TODO fix refined taskwise similarity from hierarchical decomposition
+# image(cor(mat)) 
+
+# graph <- as.dist(G, diag = TRUE)
+graph <- dist(as.matrix(G), diag = F)
+graph <- hclust(graph, method = "complete")
+# graph <- hclust(graph, method = "average") # TODO try this
+graph <- as.dendrogram(graph) 
+
+lbl <- str_sort(names(G), numeric = TRUE) # more sensible label order
+
+wt <- d$gamma %>%
+  lapply(., function(x) x[[1]][[1]]) %>% # get phenotype weight for nodes
+  lapply(., as.data.frame) %>%
+  rbindlist(.) %>%
+  unlist()
+
+graph %>%
+  set("nodes_pch", 1) %>%
+  set("labels", names(G)) %>%
+  assign_values_to_nodes_nodePar(value = 3*unlist(wt), nodePar = "cex") %>%
+  set("branches_lwd", 1+scale(d$delta[1:17])) %>%
+  dendextend::rotate(lbl) %>%
+  # hang.dendrogram %>%
+  plot()
+
+### better corrplot and distance matrix visualization
+G <- as.matrix(G)
+rownames(G) <- colnames(G)
+G <- G[lbl,lbl] # fix order
+G2 <- as.matrix(dist(G))
+p <- corrplot(as.matrix(G2), 
+              method = "color", 
+              cl.lim=c(0,1),
+              col = colorRampPalette(c("blue", "white", "red"))(200),
+              type = "full", 
+              diag = FALSE)
+
+library(qgraph)
+qgraph(1-G2, 
+       labels = colnames(G2),
+       layout = "spring", 
+       theme = "gray",
+       vsize = 10)

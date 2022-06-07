e65549568e09bca6f3c4826be51aabba75b0e71a
christianbosselmann <christian.bosselmann@med.uni-tuebingen.de>
2022-06-07 13:28:37 +0200

    app
    more work on the shiny app. working example of uniform global MTMKL. To Do: Hierarchical MTMKL.

--- app/app.R
+++ app/app.R
@@ -6,29 +6,38 @@ library("librarian")
 librarian::shelf(tidyverse,
                  shiny,
                  shinyalert,
-                 openxlsx)
+                 shinythemes,
+                 openxlsx,
+                 ontologyIndex)
 
 # set up menu choices
 vec_genes <- c("SCN1A", "SCN2A", "SCN3A", "SCN4A", "SCN5A", "SCN8A", "SCN9A", "SCN10A", "SCN11A")
 vec_aa <- c("A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V")
 
+ont_hpo <- get_ontology("hp.obo.txt", 
+                        propagate_relationships = "is_a", 
+                        extract_tags = "minimal")
+list_hpo <- lapply(1:length(ont_hpo$id), function(x) paste(ont_hpo$id[[x]], ont_hpo$name[[x]], sep = " "))
+
+
 # app
 shinyApp(
   
-  fluidPage(
+  fluidPage(theme = shinytheme("flatly"),
     
     useShinyalert(), 
     
     titlePanel(
       
       tagList(
-        span(h2("prefeKt"),
+        span(h2("SCION"),
              span(actionButton("help", "", icon = icon("question")), 
                   style = "position:absolute;right:2em;top:1em"),
-             h4("predicting the functional effects of Kv mutations"),
+             h4("Predicting the functional effects of Nav variants"),
         )
       )
     ),
+    
     hr(),
     fluidRow(),
     
@@ -37,20 +46,26 @@ shinyApp(
       sidebarPanel(
         
         selectInput(inputId = "gene",
-                    label = "gene:",
-                    choices = vec_gene,
+                    label = "Gene:",
+                    choices = vec_genes),
         
         selectInput(inputId = "aa1",
-                    label = "original aa:",
-                    choices = vec_aa,
+                    label = "Reference amino acid:",
+                    choices = vec_aa),
         
         numericInput(inputId = "pos",
-                     label = "position:",
+                     label = "Protein sequence position:",
                      value = ""),
         
         selectInput(inputId = "aa2",
-                    label = "variant aa:",
-                    choices = vec_aa,
+                    label = "Variant amino acid:",
+                    choices = vec_aa),
+                    
+        selectizeInput(inputId = "hpo",
+                    label = "Phenotypic features:",
+                    choices = NULL,
+                    multiple = TRUE,
+                    options = list(placeholder = "Search by HPO ID or name")),
         
         actionButton(inputId = "click", label = "Predict"),
         
@@ -60,9 +75,13 @@ shinyApp(
       
       mainPanel(
         
+        h5(textOutput("flag_mkl")),
         h3(textOutput("prediction")),
-        plotOutput("plot"),
-        plotOutput("plot2")
+        h5(textOutput("GOF")),
+        h5(textOutput("LOF"))
+        # dataTableOutput("table")
+        # plotOutput("plot"),
+        # plotOutput("plot2")
         
       )
     )
@@ -70,6 +89,10 @@ shinyApp(
   
   function(input, output, session){
     
+    # server-side selectize
+    updateSelectizeInput(session, "hpo", choices = list_hpo, server = TRUE)
+    
+    # check if user wants to start prediction
     observeEvent(input$click, {
       
       df_in <- data.frame(gene = input$gene, 
@@ -78,23 +101,44 @@ shinyApp(
                           aa2 = input$aa2,
                           stringsAsFactors = FALSE)
       
+      df_hpo <- list(input$hpo)
+      
       source("master.R", local = TRUE)
-      source("viz.R", local = TRUE)
+      # source("viz.R", local = TRUE)
+      
+      output$flag_mkl <- renderText({
+        if(flag_mkl == TRUE){
+          "Phenotypic information provided. Predicting with multi-task multi-kernel learning."
+        }else{
+          "No phenotypic information provided. Predicting with multi-task learning."
+        }
+      })
 
       output$prediction <- renderText({
         paste(verb_out, " ", sep="\n")
       })
       
-      output$plot <- renderPlot(p)
-      
-      output$plot2 <- renderPlot(p2)
-      
-      output$newline <- renderUI({
-        HTML(paste(" ", " ", sep="<br/>"))
+      output$GOF <- renderText({
+        paste("Probability of gain-of-function:", round(out$GOF, 3), sep=" ")
       })
+      
+      output$LOF <- renderText({
+        paste("Probability of loss-of-function:", round(out$LOF, 3), sep=" ")
+      })
+      
+      # output$table <- renderDataTable(out)
+      
+      # output$plot <- renderPlot(p)
+      
+      # output$plot2 <- renderPlot(p2)
+      
+      # output$newline <- renderUI({
+      #   HTML(paste(" ", " ", sep="<br/>"))
+      # })
       
     })
     
+    # check if user looks up the FAQ
     observeEvent(input$help, {
       shinyalert(title = "FAQ", 
                  text = "
--- app/master.R
+++ app/master.R
@@ -24,14 +24,14 @@ librarian::shelf(tidyverse,
                  quiet = TRUE)
 
 # get helper fn
-source("app/func.R")
+source("func.R")
 
 # get preprocessed training data set
-train <- read_csv("app/training_data.csv") # aka "data_prep.csv"
+train <- read_csv("training_data.csv") # aka "data_prep.csv"
 y <- as.factor(train$y)
 
 # get similarity matrix
-sim_matrices <- read_csv("app/similaritymatrix.csv")
+sim_matrices <- read_csv("similaritymatrix.csv")
 rownames(sim_matrices) <- colnames(sim_matrices)
 
 sim_match <- sim_matrices %>%
@@ -40,15 +40,15 @@ sim_match <- sim_matrices %>%
   rename(k = rowname, l = name)
 
 # get pre-processing recipe
-load("app/recipe.rds")
+load("recipe.rds")
 
 # get feature lookup tables
-aa_feats <- read_tsv("app/aa_feats.tsv")
-load("app/str_feats.rda")
-cid_raw <- read_csv("app/cid.csv")
+aa_feats <- read_tsv("aa_feats.tsv")
+load("str_feats.rda")
+cid_raw <- read_csv("cid.csv")
 
 # get test data, collect data features, then merge in features
-df_in <- openxlsx::read.xlsx("app/input.xlsx") # DEBUG
+# df_in <- openxlsx::read.xlsx("input.xlsx") # DEBUG
 n_test <- nrow(df_in)
 n_train <- nrow(train)
 n_all <- n_test + n_train
@@ -59,7 +59,7 @@ test <- df_in %>%
 
 # split, pivot and merge canonical alignment id (cid) and annotation features
 cid_align <- cid_raw[,1:10] # split into domain annotation and actual alignment
-cid_feats <- cid_raw[,10:15] # sorry, this is currently a magic number
+cid_feats <- cid_raw[,10:15] # this is currently a magic number
 
 cid_align <- cid_align %>%
   pivot_longer(!cid, names_to = "gene", values_to = "pos") %>%
@@ -95,9 +95,9 @@ t_test <- test$gene %>%
 test <- test %>%
   select(-gene)
 
-Kb <- as.data.frame(
-  kernelMatrix(
-    rbfdot(sigma = 0.01),
+Kb <- as.matrix(
+  kernlab::kernelMatrix(
+    kernlab::rbfdot(sigma = 0.01),
     as.matrix(rbind(train, test))
   ))
 
@@ -109,83 +109,92 @@ for (i in 1:n_all) {
   for (j in 1:n_all) { 
     Kt[i,j] <- sim_match$value[sim_match$k == c(t_train, t_test)[[i]] & 
                                  sim_match$l == c(t_train, t_test)[[j]]] 
-    }
+  }
 }
 
 # calculate MTL kernel Km
 Km <- as.matrix(Kb) * as.matrix(Kt)
 
-# get set of hpo terms for each training observation
-load("app/set_terms.RData")
+# load phenotypic data from user interface
+test_terms <- str_split(df_hpo, " ", 2) %>%
+  lapply(., function(x) x[1]) %>%
+  unlist()
 
 # load ontology
-ont_hpo <- get_ontology("app/hp.obo.txt", 
+ont_hpo <- get_ontology("hp.obo.txt", 
                         propagate_relationships = "is_a", 
                         extract_tags = "minimal")
 
-### TODO pheno kernel deploy
+# assert that we extracted valid HPO terms before proceeding
+if(all(test_terms %in% ont_hpo$id) == FALSE){
+  flag_mkl <- FALSE # proceed with MTL
+}else{
+  flag_mkl <- TRUE # proceed with MTMKL
+}
 
-# # propagate terms to parents
-# list_prop <- lapply(set_terms, propagate_relations, ontology = ont_hpo, relations = "parents")
-# 
-# # reshape into a wide dataframe
-# df_prop <- list_prop %>% 
-#   map_dfr(~ .x %>% as_tibble(), .id = "name")
-# 
-# df_prop <- reshape2::dcast(df_prop, as.numeric(name) ~ value, length)
-# 
-# # remove id column, redundant to rowname
-# df_prop <- df_prop[,-1]
-# 
-# # make sure that the matrix is binary (artifact from propagation)
-# df_prop[df_prop > 0] <- 1
-# 
-# # calculate pairwise jaccard coefficient
-# mat_pheno <- jaccard.test.pairwise(as.matrix(df_prop), compute.qvalue = FALSE)
-# 
-# # force to a square symmetric matrix and set diagonal to 1 (identity matrix)
-# mat_pheno <- forceSymmetric(mat_pheno$statistics)
-# diag(mat_pheno) <- 1
-# 
-# # fix class, and we are done!
-# mat_pheno <- as.matrix(mat_pheno)
-# 
-# 
-# 
-# 
-# 
-# 
-# # set up class weights
-# weights <- c("GOF" = length(train$y) / sum(train$y == "GOF"),
-#              "LOF" = length(train$y) / sum(train$y == "LOF"), 
-#              "Neutral" = length(train$y) / sum(train$y == "Neutral"))
-# 
-# # set up train and test indices
-# train_indices <- 1:length(t_train)
-# test_indices <- (length(t_train)+1):(length(t_train)+length(t_test))
-# 
-# # training SVM
-# model <- e1071::svm(
-#   x = Km[c(-test_indices), c(-test_indices), drop = FALSE],
-#   y = y,
-#   cost = 1,
-#   probability = TRUE,
-#   class.weights = weights
-# )
-# 
-# # predicting test set
-# test <- as.kernelMatrix(Km[test_indices, -test_indices, drop = FALSE])
-# prediction <- predict(model, test) %>%
-#   as_tibble() %>%
-#   cbind(attr(predict(model, test, probability = TRUE), "probabilities")) %>%
-#   cbind(attr(predict(model, test, decision.value = T), "decision.values")) %>%
-#   rename(prediction = value, 
-#          prob.LOF = LOF, prob.Neutral = Neutral, prob.GOF = GOF, 
-#          `conf.LOF_Neutral` = `LOF/Neutral`, `conf.LOF_GOF` = `LOF/GOF`, `conf.Neutral_GOF` = `Neutral/GOF`)
-# 
-# # print output
-# out <- cbind(input, prediction)
-# write.xlsx(out, "output.xlsx")
-# 
-# # verbose output for app
-# verb_out <- paste("This variant is predicted to be:", out$prediction, sep = " ")
+if(flag_mkl == TRUE) {
+  
+  # get set of hpo terms for each training observation
+  load("set_terms.RData")
+  
+  # append test terms
+  set_terms <- append(set_terms, list(test_terms))
+
+  # propagate terms to parents
+  list_prop <- lapply(set_terms, propagate_relations, ontology = ont_hpo, relations = "parents")
+
+  # reshape into a wide dataframe
+  df_prop <- list_prop %>%
+    map_dfr(~ .x %>% as_tibble(), .id = "name")
+
+  df_prop <- reshape2::dcast(df_prop, as.numeric(name) ~ value, length)
+
+  # remove id column, redundant to rowname
+  df_prop <- df_prop[,-1]
+
+  # make sure that the matrix is binary (artifact from propagation)
+  df_prop[df_prop > 0] <- 1
+
+  # calculate pairwise jaccard coefficient
+  mat_pheno <- jaccard.test.pairwise(as.matrix(df_prop), compute.qvalue = FALSE)
+
+  # force to a square symmetric matrix and set diagonal to 1 (identity matrix)
+  mat_pheno <- forceSymmetric(mat_pheno$statistics)
+  diag(mat_pheno) <- 1
+
+  # fix type
+  mat_pheno <- as.matrix(mat_pheno)
+  
+  # scale and center kernel matrices
+  Kb <- kernelPreparation(Kb) # instance-level sequence and structure
+  Km <- kernelPreparation(mat_pheno) # instance-level phenotype
+  Kt <- kernelPreparation(Kt) # instance-level task similarity
+  Km <- Kb+Km+Kt
+}
+
+# set up train and test indices
+train_indices <- 1:length(t_train)
+test_indices <- (length(t_train)+1):(length(t_train)+length(t_test))
+
+# training SVM
+model <- e1071::svm(
+  x = Km[c(-test_indices), c(-test_indices), drop = FALSE],
+  y = y,
+  cost = 4,
+  probability = TRUE
+)
+
+# predicting test set
+test <- as.kernelMatrix(Km[test_indices, -test_indices, drop = FALSE])
+prediction <- predict(model, test) %>%
+  as_tibble() %>%
+  cbind(attr(predict(model, test, probability = TRUE), "probabilities")) %>%
+  cbind(attr(predict(model, test, decision.value = TRUE), "decision.values")) 
+
+# print output
+out <- cbind(df_in, prediction)
+# write.xlsx(out, "output.xlsx") # DEBUG
+
+# verbose output for app
+verb_name <- paste(df_in$gene, paste("p.", paste(df_in$aa1, df_in$pos, df_in$aa2, sep = ""), sep = ""), sep = " ")
+verb_out <- paste(verb_name, "is predicted to be:", out$value, sep = " ")
